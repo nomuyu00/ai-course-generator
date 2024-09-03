9d23959e020d4fd8e0dbf33c70828ba4f49a434c
nomuyu <nomuyu.zero@gmail.com>
2024-09-03 17:14:31 +0900

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      {/* HEADER */}
+
+
+      {/* Hero Section */}
+
+    </div>
+  );
+}
